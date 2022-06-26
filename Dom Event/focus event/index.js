7cce53c241271d3ec59a014019c64c60cad8a8a5
mithun399 <msovik399@gmail.com>
2022-06-26 13:35:49 +0600

    focus event

---
+++ Dom Event/focus event/index.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("input");
+input.addEventListener("blur", function (e) {
+  //   console.log(e.target.value);
+  input.value = e.target.value.toUpperCase();
+});
+input.addEventListener("focus", function () {
+  //   console.log("focus is occured");
+  //   input.style.backgroundColor = "orange";
+  //   input.style.padding = "2rem";
+});
+// input.addEventListener("focusin", function () {
+//   console.log("focusin is occured");
+// });
+// input.addEventListener("focusout", function () {
+//   console.log("focusout is occured");
+// });
